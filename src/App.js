dfc9b78f70c407db3e926171440c4a92cf0a5a11
unknown <olena.denysova88@gmail.com>
2021-12-09 22:08:47 +0100

    old routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import './App.css';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      
+      <main className='container content'>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
+        </BrowserRouter>
+      </main>
       <Footer />
     </div>
   );
