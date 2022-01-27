b349ae74b3af3f9142371f462adfcee8de804cb8
unknown <olena.denysova88@gmail.com>
2022-01-27 11:59:33 +0100

    footer and header tests

---
+++ src/components/Footer/Footer.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Footer from "./Footer";
+
+describe("Footer component:", () => {
+    beforeEach(() => {
+        render(<Footer />);
+    });
+
+    test("copyright rendering", () => {
+        const author = screen.getByText('Copyright Elena Riemer', { exact: false });
+        expect(author).toBeInTheDocument;
+    });
+
+    test("copyright rendering correct year", () => {
+        const author = screen.getByText('Copyright Elena Riemer', { exact: false });
+        const year = new Date().getFullYear();
+        
+        expect(author).toHaveTextContent(year);
+    });
+
+    test("top button should be enabled", () => {
+        const topButton = screen.getByRole("button", {
+            name: /go to top/i,
+        });
+        expect(topButton).toBeEnabled;
+    });
+});
---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../../App";
+
+describe("Header component:", () => {
+    beforeEach(() => {
+        render(<App />);
+    });
+
+    test("welcome text", () => {
+        const text = screen.getByText(/what would you like to cook today\?/i);
+        expect(text).toBeInTheDocument;
+    });
+
+    test("logo", () => {
+        const logo = screen.getByRole("img", { name: /logo/i });
+        expect(logo).not.toBeNull;
+        expect(logo).toBeInTheDocument;
+    });
+
+    test("menu", () => {
+        const homeLink = screen.getByRole("link", { name: /home/i });
+        const aboutLink = screen.getByRole("link", { name: /about/i });
+        expect(homeLink).toBeInTheDocument;
+        expect(aboutLink).toBeInTheDocument;
+    });
+
+    test("gitHub link", () => {
+        const gitHubLink = screen.getByRole("img", { name: /github link/i });
+        expect(gitHubLink).toBeInTheDocument;
+    });
+});
